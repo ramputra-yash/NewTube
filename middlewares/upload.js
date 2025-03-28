5b2d123848210ea48a807e1119e65beac709128f
Yogesh Sharma <155828455+ramputra-yash@users.noreply.github.com>
2025-03-28 10:45:12 +0530

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,15 +1,26 @@
-const multer  = require('multer')
-const path = require('path');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+require('dotenv').config();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/videos/')
+// Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Cloudinary Storage for Videos
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: 'user_videos', // Videos ke liye alag folder
+      resource_type: 'video', // Video files ke liye
+      public_id: `${Date.now()}-${file.originalname.replace(/\s+/g, '-').replace(/[^\w.-]/g, '')}`,
+      format: ['mp4', 'mkv', 'avi', 'mov'], // Allowed formats
+    };
   },
-  filename: function (req, file, cb) {
-    const fn = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, fn + path.extname(file.originalname))
-  }
-})
-
+});
 const upload = multer({ storage: storage })
 module.exports = upload;
